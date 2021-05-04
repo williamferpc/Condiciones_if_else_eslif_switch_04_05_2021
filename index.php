eceda0b1e6fa04a95a87ba5435d61e1a6878909b
williamferpc <willi_9212@hotmail.com>
2021-05-04 16:50:34 -0500

    condicionales 04_05_2021

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php 
+
+    
+
+    //Creamos un formulario que pida la edad 
+    //Crea un condiconales que segun la edad muestre si es 
+    //Mayor de edad,un recien nacido, un niño(a), un adolecente, un adulto(a)
+    //un señor(a), o un anciano 04-05-2021
+
+
+//     $num1 = (int) 2;
+//     if($num1 > 0 && $num1 <= 5){
+        
+//         if($num1==1){
+//             echo <<<NumeroEsUno
+//             El numero $num1 es igual un 1
+// NumeroEsUno;
+//         }elseif($num1==2){
+//             echo <<<NumeroEsUno
+//             El numero $num1 es igual a 2
+// NumeroEsUno;
+//         }else{
+//             echo <<<NumeroEsUno
+//             El numero $num1 es mayor o igual a 0
+// NumeroEsUno;
+//         }
+
+//     }elseif($num1 == 0){
+//         echo <<<NumeroEsIgual0
+//         El numero $num1 es igual a 0
+// NumeroEsIgual0;
+//     }elseif($num1 < 0 && $num1 >= -5){
+//         echo <<<NumeroEsnegativo
+//         El numero $num1 es es negativo
+// NumeroEsnegativo;
+//     }else{
+//         echo <<<ProbabilidadNoExistente
+//         El numero $num1 no tiene una condicion o una probabilidad
+// ProbabilidadNoExistente;
+//     }
+
+?>
